9f8cd39e491ac28856ec49f09285a60afb565265
DavidAce <aceituno@kth.se>
2026-03-27 13:57:57 +0100

    Updated the docs toolchain

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,18 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-# -- Read The Docs setup --------------------------------------------------------------
-
-import os
-import subprocess
-
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if on_rtd:
-    subprocess.call('cd ..; doxygen', shell=True)
-
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'DMRG++'
@@ -73,15 +61,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
 
 
