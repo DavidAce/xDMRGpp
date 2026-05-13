6d48d2cad3f2d979ca715385edb79f07519f70a8
DavidAce <aceituno@kth.se>
2026-05-13 11:16:29 +0200

    Fix asScalarType test ownership
    - Exercise tenx::asScalarType directly instead of a local copy.
    - Check lvalue reference preservation.
    - Check rvalue conversion returns an owning tensor.

--- tests/asScalarType/main.cpp
+++ tests/asScalarType/main.cpp
@@ -10,29 +10,6 @@
 #include <stdfloat>
 #include <unsupported/Eigen/CXX11/Tensor>
 
-template<typename Scalar, typename T>
-requires tenx::sfinae::is_eigen_tensor_v<std::remove_cvref_t<T>>
-decltype(auto) asScalarType2(T &&tensor) {
-    static_assert(sfinae::is_any_v<Scalar, fp32, fp64, fp128, cx32, cx64, cx128>);
-    using EigenType = std::remove_cvref_t<decltype(tensor)>;
-    using OldScalar = typename EigenType::Scalar;
-    fmt::print("T      {}\n", sfinae::type_name<T>());
-    fmt::print("tensor {}\n", sfinae::type_name<decltype(tensor)>());
-    if constexpr(std::is_same_v<OldScalar, Scalar>) {
-        return (tensor); // Returns the exact same object as a reference
-    } else if constexpr(sfinae::is_std_complex_v<OldScalar> and !sfinae::is_std_complex_v<Scalar>) {
-        // Complex to Real
-        using DimType       = typename EigenType::Dimensions;
-        constexpr auto rank = Eigen::internal::array_size<DimType>::value;
-        return Eigen::Tensor<Scalar, rank>(tensor.real().template cast<Scalar>());
-    } else {
-        // Cast between different precisions, e.g. fp64 to fp32
-        using DimType       = typename EigenType::Dimensions;
-        constexpr auto rank = Eigen::internal::array_size<DimType>::value;
-        return Eigen::Tensor<Scalar, rank>(tensor.template cast<Scalar>());
-    }
-}
-
 template<typename Scalar>
 struct MpoSite {
     Eigen::Tensor<Scalar, 1> tensor = {};
@@ -48,14 +25,21 @@ struct MpoSite {
     }
     template<typename T>
     decltype(auto) get_tensor() {
-        return asScalarType2<T>(get_tensor(tensor));
+        return tenx::asScalarType<T>(get_tensor(tensor));
     }
 };
 
 int main() {
     auto mpo    = MpoSite<cx64>();
     auto tensor = Eigen::Tensor<double, 1>(1000000);
-    // fmt::print("{} : {}\n", sfinae::type_name<decltype(asScalarType2<fp64>(tensor))>(), fp(asScalarType2<fp64>(tensor).coeff(0)));
+    decltype(auto) tensor_ref = tenx::asScalarType<fp64>(tensor);
+    static_assert(std::is_lvalue_reference_v<decltype(tensor_ref)>);
+    assert(&tensor_ref == &tensor);
+
+    auto tensor_copy = tenx::asScalarType<fp64>(Eigen::Tensor<double, 1>(1000000));
+    static_assert(not std::is_reference_v<decltype(tensor_copy)>);
+    assert(tensor_copy.size() == 1000000);
+
     fmt::print("{} : {}\n", sfinae::type_name<decltype(mpo.get_tensor<fp64>())>(), fp(mpo.get_tensor<double>().coeff(0)));
     fmt::print("{} : {}\n", sfinae::type_name<decltype(mpo.get_tensor<cx64>())>(), fp(mpo.get_tensor<cx64>().coeff(0)));
 
